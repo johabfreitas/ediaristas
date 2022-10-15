4fa67f8ecfaae76358185f7b4ef9379ef0429b54
Johab <freitasjohab@gmail.com>
2022-10-15 16:40:21 -0300

    Criando action de exclusão de serviços

--- src/main/java/br/com/johabfreitas/ediaristas/web/controllers/ServicoController.java
+++ src/main/java/br/com/johabfreitas/ediaristas/web/controllers/ServicoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,35 +16,43 @@ import br.com.johabfreitas.ediaristas.core.repositories.ServicoRepository;
 @Controller
 @RequestMapping("/admin/servicos")
 public class ServicoController {
-	
+
 	@Autowired
 	private ServicoRepository repository;
-	
+
 	@GetMapping
 	public ModelAndView buscarTodos() {
 		var modelAndView = new ModelAndView("admin/servico/lista");
-		
+
 		modelAndView.addObject("servicos", repository.findAll());
-		
+
 		return modelAndView;
 	}
-	
+
 	@GetMapping("/cadastrar")
 	public ModelAndView cadastrar() {
 		var modelAndView = new ModelAndView("admin/servico/form");
-		
+
 		modelAndView.addObject("servico", new Servico());
-		
+
 		return modelAndView;
 	}
-	
+
 	@PostMapping("/cadastrar")
 	public String cadastrar(Servico servico) {
 		repository.save(servico);
-		
+
 		return "redirect:/admin/servicos";
 	}
-	
+
+	@GetMapping("/{id}/excluir")
+	public String excluir(@PathVariable Long id) {
+		repository.deleteById(id);
+
+		return "redirect:/admin/servicos";
+
+	}
+
 	@ModelAttribute("icones")
 	public Icone[] getIcones() {
 		return Icone.values();
