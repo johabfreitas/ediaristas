4e2bd215ff3d10ccc3df908757487122e9219f7b
Johab <freitasjohab@gmail.com>
2022-10-14 22:00:47 -0300

    First commit

---
+++ src/main/java/br/com/johabfreitas/ediaristas/core/repositories/ServicoRepository.java
@@ -0,0 +1,9 @@
+package br.com.johabfreitas.ediaristas.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.johabfreitas.ediaristas.core.models.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long>{
+
+}
