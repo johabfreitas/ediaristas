21bbe989fdc33f6f84655264b175017a14994b73
Johab <freitasjohab@gmail.com>
2022-10-30 00:42:25 -0300

    Criando o Mapper de serviço

---
+++ src/main/java/br/com/johabfreitas/ediaristas/web/mappers/WebServicoMapper.java
@@ -0,0 +1,65 @@
+package br.com.johabfreitas.ediaristas.web.mappers;
+
+import org.springframework.stereotype.Component;
+
+import br.com.johabfreitas.ediaristas.core.models.Servico;
+import br.com.johabfreitas.ediaristas.web.dtos.ServicoForm;
+
+@Component
+public class WebServicoMapper {
+
+	public Servico toModel(ServicoForm form) {
+		if(form == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		var model = new Servico();
+		
+		model.setNome(form.getNome());
+		model.setValorMinimo(form.getValorMinimo());
+		model.setQtdHoras(form.getQtdHoras());
+		model.setPorcentagemComissao(form.getPorcentagemComissao());
+		model.setHorasQuarto(form.getHorasQuarto());
+		model.setValorQuarto(form.getValorQuarto());
+		model.setHorasSala(form.getHorasSala());
+		model.setValorSala(form.getValorSala());
+		model.setHorasBanheiro(form.getHorasBanheiro());
+		model.setValorBanheiro(form.getValorBanheiro());
+		model.setHorasCozinha(form.getHorasCozinha());
+		model.setValorCozinha(form.getValorCozinha());
+		model.setHorasQuintal(form.getHorasQuintal());
+		model.setHorasOutros(form.getHorasOutros());
+		model.setIcone(form.getIcone());
+		model.setPosicao(form.getPosicao());
+		
+		return model;
+	}
+	
+	public ServicoForm toForm(Servico model) {
+		if(model == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		var form  = new ServicoForm();
+		
+		form.setNome(model.getNome());
+		form.setValorMinimo(model.getValorMinimo());
+		form.setQtdHoras(model.getQtdHoras());
+		form.setPorcentagemComissao(model.getPorcentagemComissao());
+		form.setHorasQuarto(model.getHorasQuarto());
+		form.setValorQuarto(model.getValorQuarto());
+		form.setHorasSala(model.getHorasSala());
+		form.setValorSala(model.getValorSala());
+		form.setHorasBanheiro(model.getHorasBanheiro());
+		form.setValorBanheiro(model.getValorBanheiro());
+		form.setHorasCozinha(model.getHorasCozinha());
+		form.setValorCozinha(model.getValorCozinha());
+		form.setHorasQuintal(model.getHorasQuintal());
+		form.setHorasOutros(model.getHorasOutros());
+		form.setIcone(model.getIcone());
+		form.setPosicao(model.getPosicao());
+		
+		return form;
+	}
+	
+}
