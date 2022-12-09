fb167b3055c5dc6381c897bfd06343dad0432f64
Johab <freitasjohab@gmail.com>
2022-12-09 19:41:02 -0300

    Funcionalidade de excluir um usuário

--- src/main/java/br/com/johabfreitas/ediaristas/core/exceptions/UsuarioNaoEncontradoException.java
+++ src/main/java/br/com/johabfreitas/ediaristas/core/exceptions/UsuarioNaoEncontradoException.java
@@ -2,11 +2,11 @@ package br.com.johabfreitas.ediaristas.core.exceptions;
 
 import javax.persistence.EntityNotFoundException;
 
-public class ServicoNaoEncontradoException extends EntityNotFoundException{
+public class UsuarioNaoEncontradoException extends EntityNotFoundException {
 	
 	private static final long serialVersionUID = 1L;
 
-	public ServicoNaoEncontradoException(String message) {
+	public UsuarioNaoEncontradoException(String message) {
 		super(message);
 	}
 
